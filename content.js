0882e98d2fd5de282c638f4e4ef1c60a86a4b950
Tanishk Tiwari <tatiwari@microsoft.com>
2020-07-23 00:23:48 +0530

    Add web-component for search-results, refractor

--- content.js
+++ content.js
@@ -1,10 +1,11 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      if( request.greeting === "fetch_current_url" ) {
-        var url = document.URL;
-  
-      //alert("i fetched");
-        chrome.runtime.sendMessage({"greeting": "save_current_url", "url": url});
-      }
+        if (request.greeting === "fetch_current_url") {
+            var url = document.URL;
+            chrome.runtime.sendMessage({
+                "greeting": "save_current_url",
+                "url": url
+            });
+        }
     }
-  );
+);
