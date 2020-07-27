0c8eca8e035408ce031e106166275c512a26888d
Tanishk Tiwari <tatiwari@microsoft.com>
2020-07-27 18:59:07 +0530

    Add web-component

--- popup.js
+++ popup.js
@@ -1,24 +1,95 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector("#getAllBookmarks").addEventListener('click', openBookmarkFunction);
-    document.querySelector("#addBookmarks").addEventListener('click', fetchCurrentUrl);
+document.addEventListener('DOMContentLoaded', setListenerForSearch);
 
-    // Add listener for "Enter" for our search-box.
+function setListenerForSearch() {
+    document.querySelector(".getAllBookmarks").addEventListener('click', openBookmark);
+    document.querySelector(".addBookmarks").addEventListener('click', addCurrentURL);
     document.getElementById('omnibox').addEventListener('keyup', searchForTag);
+    // alert("added for search");
+}
 
-});
+function clearSearchListener() {
+    document.querySelector(".getAllBookmarks").removeEventListener('click', openBookmark);
+    document.querySelector(".addBookmarks").removeEventListener('click', addCurrentURL);
+    document.getElementById('omnibox').removeEventListener('keyup', searchForTag);
+    // alert("cleared for search");
+}
 
-function openBookmarkFunction() {
-    // Use settings here ?
+function setListenerForBookmark() {
+    document.querySelector(".getAllBookmarks").addEventListener('click', saveBookmark);
+    document.querySelector(".addBookmarks").addEventListener('click', goToSearchView);
+    // alert("Added For bookmarks");
+}
+
+function clearBookmarkListener() {
+    document.querySelector(".getAllBookmarks").removeEventListener('click', saveBookmark);
+    document.querySelector(".addBookmarks").removeEventListener('click', goToSearchView);
+}
+
+function goToSearchView() {
+    alert("going back....")
+}
+
+function saveBookmark() {
+    alert("saved");
+}
+
+function openBookmark() {
     console.log("open button");
     chrome.tabs.create({
         "url": "https://callstack.com/blog/say-goodbye-to-old-fashioned-forks-thanks-to-the-patch-package/"
     });
 }
 
+function toggleToBookmarkButtons() {
+    let saveButton = document.querySelector('.getAllBookmarks');
+    let addButton = document.querySelector('.addBookmarks');
+
+    if (saveButton.innerHTML === "Save") {
+        saveButton.innerHTML = "See All";
+        addButton.innerHTML = "Add New";
+        saveButton.style.backgroundColor = "#666";
+        saveButton.style.color = "white";
+    } else {
+        saveButton.innerHTML = "Save";
+        addButton.innerHTML = "Back";
+        saveButton.style.backgroundColor = "#00dfff";
+        saveButton.style.color = "black";
+    }
+}
+
+function addCurrentURL() {
+    // Hide results container.
+    document.querySelector('.resultsContainer').style.display = "none";
+
+    // Clear input-text field
+    document.getElementById("omnibox").value = "";
+
+    // Show notes input-text
+    let notesInput = document.createElement("input");
+    notesInput.type = "text";
+    notesInput.id = "notesBox";
+
+    let omniboxContainer = document.querySelector('.omniboxContainer');
+    omniboxContainer.appendChild(document.createElement("br"));
+    omniboxContainer.appendChild(notesInput);
+
+    // GetCurrent url and Page title
+
+    // Add correct listeners
+    clearSearchListener();
+    setListenerForBookmark();
+
+    // Show page-title in editable omniboxContainer
+
+    // Change button-texts
+    toggleToBookmarkButtons();
+}
+
 function fetchCurrentUrl() {
     chrome.runtime.sendMessage({
         "greeting": "from_popup_to_fetch_url"
     });
+    
 }
 
 chrome.runtime.onMessage.addListener(
@@ -32,6 +103,6 @@ chrome.runtime.onMessage.addListener(
 function searchForTag(event) {
     if (event.code !== "Enter")
         return;
-    // Fired only on enter-press
     var result = event.target.value;
+    alert(result);
 }
---
+++ searchcard.js
@@ -0,0 +1,91 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+  .container {
+		background: grey;
+		width: 250px;
+		display: grid;
+		grid-template-columns: 1fr 3fr;
+		grid-gap: 6px;
+		margin-bottom: 5px;
+        border-radius: 3px;
+        padding: 6px;
+	}
+
+	.img > img {
+		width: 100%;
+        border-radius: 3px;
+    }
+
+    .img {
+        text-align:center;
+        margin: auto auto;
+    }
+
+    .bookmark {
+        display:grid;
+        grid-gap: 2px;
+    }
+
+    .bookmark > div {
+        background-color:green; /* for visibility*/
+    }
+
+    .title {
+        font-size:12px;
+        height: 30px;
+        text-align: center;
+        line-height:12px;
+    }
+
+    .notes {
+        height: 30px;
+        font-size:12px;
+        text-align:center;
+        line-height:12px;
+    }
+
+  </style>
+
+  <div class="container">
+
+    <div class="img">
+        <img/>
+    </div>
+
+    <div class="bookmark">
+        <div class="title">
+            <h3 id="bookmarkTitle"></h3>
+        </div>
+        <div class="notes">
+            <p><slot name="notes" /></p>
+        </div>
+    </div>
+
+  </div>
+`;
+
+class UserCard extends HTMLElement {
+    constructor() {
+        super();
+        // this custom web-element is encapsulated under shadow dom,
+        // i.e. global css and sselectors won't work on |UserCard| elements.
+        this.attachShadow({
+            mode: 'open'
+        });
+        // Use the above declared template to make new custom elemennts.
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        // < user-card title="..." >
+        // Extracts the content passed into |title| of our custom-element
+        // and pastes it under the <h3> of our custom-element.
+        this.shadowRoot.querySelector('h3').innerText = this.getAttribute('title');
+        this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
+
+        // P.S Note that values under <slots> of our custom-element are
+        // directly passed from html.
+    }
+}
+
+// Define the custom element for use.
+window.customElements.define('user-card', UserCard);
