96ddc84bba0826ead04823fc3a1e715bf42b01ed
Tanishk Tiwari <tatiwari@microsoft.com>
2020-07-29 23:25:20 +0530

    Add click to open link from extension-view

--- searchcard.js
+++ searchcard.js
@@ -1,8 +1,7 @@
 'use strict';
 
-(function() {
-    const template = document.createElement('template');
-    template.innerHTML = `
+const template = document.createElement('template');
+template.innerHTML = `
   <style>
   .container {
 		background: grey;
@@ -16,11 +15,18 @@
 	}
 
 	.img > img {
-		width: 100%;
-        border-radius: 3px;
+		width: 50%;
+        object-fit:center;
     }
 
     .img {
+        height: 50px;
+        width: 50px;
+        border-radius: 25px;
+        background: #333;
+        display: flex;
+        align-items: center;
+        justify-content: center;
         text-align:center;
         margin: auto auto;
     }
@@ -28,10 +34,8 @@
     .bookmark {
         display:grid;
         grid-gap: 2px;
-    }
-
-    .bookmark > div {
-        background-color:green; /* for visibility*/
+        background-color:green;
+        border-radius:3px;
     }
 
     .title {
@@ -44,7 +48,14 @@
         white-space: nowrap;
     }
 
+    .title:hover {
+        cursor: pointer;
+        color: blue;
+        text-decoration: underline;
+    }
+
     .notes {
+        cursor:default;
         height: 30px;
         font-size:12px;
         text-align:center;
@@ -73,38 +84,40 @@
   </div>
 `;
 
-    class UserCard extends HTMLElement {
-        set title(val) {
-            this.setAttribute('title', val);
-            this.shadowRoot.querySelector('h3').innerText = val;
-        }
-
-        set avatar(val) {
-            this.setAttribute('avatar', val);
-            this.shadowRoot.querySelector('img').src = val;
-        }
-
-        constructor() {
-            super();
-            // this custom web-element is encapsulated under shadow dom,
-            // i.e. global css and sselectors won't work on |UserCard| elements.
-            this.attachShadow({
-                mode: 'open'
-            });
-            // Use the above declared template to make new custom elemennts.
-            this.shadowRoot.appendChild(template.content.cloneNode(true));
-
-            // < user-card title="..." >
-            // Extracts the content passed into |title| of our custom-element
-            // and pastes it under the <h3> of our custom-element.
-            this.shadowRoot.querySelector('h3').innerText = this.getAttribute('title');
-            this.shadowRoot.querySelector('img').src = this.getAttribute('avatar');
-
-            // P.S Note that values under <slots> of our custom-element are
-            // directly passed from html.
-        }
+class UserCard extends HTMLElement {
+    set title(val) {
+        this.setAttribute('title', val);
+        this.shadowRoot.querySelector('h3').innerText = val;
     }
 
-    // Define the custom element for use.
-    window.customElements.define('user-card', UserCard);
-})();
+    set avatar(val) {
+        this.setAttribute('avatar', val);
+        this.shadowRoot.querySelector('img').src = "https://s2.googleusercontent.com/s2/favicons?domain_url=" + val;
+    }
+
+    constructor(onBookmarkClickCallback) {
+        super();
+        // this custom web-element is encapsulated under shadow dom,
+        // i.e. global css and sselectors won't work on |UserCard| elements.
+        this.attachShadow({
+            mode: 'open'
+        });
+        // Use the above declared template to make new custom elemennts.
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        // < user-card title="..." >
+        // Extracts the content passed into |title| of our custom-element
+        // and pastes it under the <h3> of our custom-element.
+        let bookmark_title = this.shadowRoot.querySelector('#bookmarkTitle');
+
+        bookmark_title.addEventListener('click', e => {
+            onBookmarkClickCallback(this.getAttribute('avatar'));
+        });
+
+        // P.S Note that values under <slots> of our custom-element are
+        // directly passed from html.
+    }
+}
+
+// Define the custom element for use.
+window.customElements.define('user-card', UserCard);
